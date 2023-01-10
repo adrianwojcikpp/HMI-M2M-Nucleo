7734a44d2bd1e82083f6186d7059bbc1bed722c5
Adrian Wójcik <54798218+adrianwojcikpp@users.noreply.github.com>
2023-01-10 10:53:48 +0100

    CubeMX FreeRTOS config

--- NUCLEO-F746ZG/DemoApp07_LwIP/Core/Src/freertos.c
+++ NUCLEO-F746ZG/DemoApp07_LwIP/Core/Src/freertos.c
@@ -25,7 +25,9 @@
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-
+#include "lwip/opt.h"
+#include "lwip/api.h"
+#include "lwip/sys.h"
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -48,14 +50,18 @@
 
 /* USER CODE END Variables */
 osThreadId defaultTaskHandle;
+osThreadId udpechoTaskHandle;
+osThreadId tcpechoTaskHandle;
 
 /* Private function prototypes -----------------------------------------------*/
 /* USER CODE BEGIN FunctionPrototypes */
-extern void tcpecho_init(void);
-extern void udpecho_init(void);
+//extern void tcpecho_init(void);
+//extern void udpecho_init(void);
 /* USER CODE END FunctionPrototypes */
 
 void StartDefaultTask(void const * argument);
+void StartUdpEcho(void const * argument);
+void StartTcpEcho(void const * argument);
 
 extern void MX_LWIP_Init(void);
 void MX_FREERTOS_Init(void); /* (MISRA C 2004 rule 8.1) */
@@ -107,6 +113,14 @@ void MX_FREERTOS_Init(void) {
   osThreadDef(defaultTask, StartDefaultTask, osPriorityNormal, 0, 256);
   defaultTaskHandle = osThreadCreate(osThread(defaultTask), NULL);
 
+  /* definition and creation of udpechoTask */
+  osThreadDef(udpechoTask, StartUdpEcho, osPriorityRealtime, 0, 1024);
+  udpechoTaskHandle = osThreadCreate(osThread(udpechoTask), NULL);
+
+  /* definition and creation of tcpechoTask */
+  osThreadDef(tcpechoTask, StartTcpEcho, osPriorityRealtime, 0, 1024);
+  tcpechoTaskHandle = osThreadCreate(osThread(tcpechoTask), NULL);
+
   /* USER CODE BEGIN RTOS_THREADS */
   /* add threads, ... */
   /* USER CODE END RTOS_THREADS */
@@ -128,7 +142,7 @@ void StartDefaultTask(void const * argument)
 #if TCP_SERVER
   tcpecho_init();
 #else
-  udpecho_init();
+  //udpecho_init();
 #endif
   /* Infinite loop */
   for(;;)
@@ -137,6 +151,141 @@ void StartDefaultTask(void const * argument)
     osDelay(100);
   }
   /* USER CODE END StartDefaultTask */
+}
+
+/* USER CODE BEGIN Header_StartUdpEcho */
+/**
+* @brief Function implementing the udpechoTask thread.
+* @param argument: Not used
+* @retval None
+*/
+/* USER CODE END Header_StartUdpEcho */
+void StartUdpEcho(void const * argument)
+{
+  /* USER CODE BEGIN StartUdpEcho */
+  struct netconn *conn;
+  struct netbuf *buf;
+  char buffer[128];
+  err_t err;
+  LWIP_UNUSED_ARG(argument);
+
+  osDelay(100); // TODO: add semaphore
+
+#if LWIP_IPV6
+  conn = netconn_new(NETCONN_UDP_IPV6);
+  LWIP_ERROR("udpecho: invalid conn", (conn != NULL), return;);
+  netconn_bind(conn, IP6_ADDR_ANY, 8);
+#else /* LWIP_IPV6 */
+  conn = netconn_new(NETCONN_UDP);
+  LWIP_ERROR("udpecho: invalid conn", (conn != NULL), return;);
+  netconn_bind(conn, IP_ADDR_ANY, 8);
+#endif /* LWIP_IPV6 */
+  /* Infinite loop */
+  for(;;)
+  {
+	err = netconn_recv(conn, &buf);
+	if (err == ERR_OK)
+	{
+	  HAL_GPIO_WritePin(LD2_GPIO_Port, LD2_Pin, 1);
+	  /*  no need netconn_connect here, since the netbuf contains the address */
+      if(netbuf_copy(buf, buffer, sizeof(buffer)) != buf->p->tot_len)
+	  {
+        LWIP_DEBUGF(LWIP_DBG_ON, ("netbuf_copy failed\n"));
+	  }
+	  else
+	  {
+		buffer[buf->p->tot_len] = '\0';
+		err = netconn_send(conn, buf);
+
+		if(err != ERR_OK)
+		{
+		  LWIP_DEBUGF(LWIP_DBG_ON, ("netconn_send failed: %d\n", (int)err));
+		}
+		else
+		{
+		  LWIP_DEBUGF(LWIP_DBG_ON, ("got %s\n", buffer));
+		}
+	  }
+	  netbuf_delete(buf);
+	  HAL_GPIO_WritePin(LD2_GPIO_Port, LD2_Pin, 0);
+	}
+  }
+  /* USER CODE END StartUdpEcho */
+}
+
+/* USER CODE BEGIN Header_StartTcpEcho */
+/**
+* @brief Function implementing the tcpechoTask thread.
+* @param argument: Not used
+* @retval None
+*/
+/* USER CODE END Header_StartTcpEcho */
+void StartTcpEcho(void const * argument)
+{
+  /* USER CODE BEGIN StartTcpEcho */
+  struct netconn *conn, *newconn;
+  err_t err, accept_err;
+  struct netbuf *buf;
+  void *data;
+  u16_t len;
+  int msg_cnt = 0;
+  LWIP_UNUSED_ARG(argument);
+
+  osDelay(100); // TODO: add semaphore
+
+  /* Create a new connection identifier. */
+  conn = netconn_new(NETCONN_TCP);
+
+  if (conn!=NULL)
+  {
+	/* Bind connection to well known port number 7. */
+	err = netconn_bind(conn, NULL, 7);
+
+	if (err == ERR_OK)
+	{
+	  /* Tell connection to go into listening mode. */
+	  netconn_listen(conn);
+      /* Infinite loop */
+	  for(;;)
+	  {
+
+		/* Grab new connection. */
+		accept_err = netconn_accept(conn, &newconn);
+
+		/* Process the new connection. */
+		if (accept_err == ERR_OK)
+		{
+
+		  while (netconn_recv(newconn, &buf) == ERR_OK)
+	      {
+			HAL_GPIO_WritePin(LD3_GPIO_Port, LD3_Pin, 1);
+		    msg_cnt++;
+		    do
+		    {
+			  netbuf_data(buf, &data, &len);
+			  char prefix[32];
+			  int prefix_len = sprintf(prefix, "\r\nMessage #%03d: ", msg_cnt);
+			  netconn_write(newconn, prefix, prefix_len, NETCONN_COPY);
+			  netconn_write(newconn, data, len, NETCONN_COPY);
+			  char postfix[] = "\r\n";
+			  netconn_write(newconn, postfix, 2, NETCONN_NOFLAG);
+		    } while (netbuf_next(buf) >= 0);
+            netbuf_delete(buf);
+  		    HAL_GPIO_WritePin(LD3_GPIO_Port, LD3_Pin, 0);
+		  }
+
+		  /* Close connection and discard connection identifier. */
+		  netconn_close(newconn);
+		  netconn_delete(newconn);
+		}
+	  }
+	}
+    else
+    {
+      netconn_delete(newconn);
+    }
+  }
+  /* USER CODE END StartTcpEcho */
 }
 
 /* Private application code --------------------------------------------------*/
